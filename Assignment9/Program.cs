5ec1bb927f79d999a1474929706314450f7e902b
u29d58 <u29d58@INGURWN120082>
2023-11-02 16:55:45 +0530

    Add project files.

---
+++ Assignment9/Program.cs
@@ -0,0 +1,26 @@
+﻿using AbstractClassInterfaceDemo;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+
+        Shape shape = null;
+        Console.WriteLine("Enter shape type");
+        int c = Byte.Parse(Console.ReadLine());
+        if (c == 1)
+        {
+            shape = new Square();
+
+        }
+        else if (c == 2)
+        {
+            shape = new Rectangle();
+
+        }
+        CheckShape checkShape = new CheckShape();
+        checkShape.Checkshape(shape);
+
+
+    }
+}
